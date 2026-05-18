c1cbee8bbc25e714d9a5f21c48def570153737be
之雨 <29673994+1299172402@users.noreply.github.com>
2026-05-18 22:07:11 +0800

    revert: 注册为服务

--- cmd/console/main.go
+++ cmd/console/main.go
@@ -4,13 +4,12 @@ import (
 	"fmt"
 
 	"Doubao-input/info"
-	"Doubao-input/internal/system"
+	"Doubao-input/internal/core"
 )
 
 func main() {
 	fmt.Printf("Doubao Input\n")
 	fmt.Printf("Version: %s\n", info.Version)
 
-	// 启动服务
-	system.RunService()
+	core.StartClipboardWriter()
 }
